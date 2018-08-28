b4b95d1bba0c7b592c2b4049908ef1487318db03
EvilCodes <huangy_tao@126.com>
2018-08-28 16:16:38 +0800

    完成有关DrawerLayout代码相关的工作

--- materialdesign2/src/main/java/com/xiaohu/materialdesign2/MainActivity.java
+++ materialdesign2/src/main/java/com/xiaohu/materialdesign2/MainActivity.java
@@ -1,13 +1,59 @@
 package com.xiaohu.materialdesign2;
 
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBar supportActionBar = getSupportActionBar();
+        if (supportActionBar != null) {
+            supportActionBar.setDisplayHomeAsUpEnabled(true);
+            supportActionBar.setHomeAsUpIndicator(R.mipmap.ic_launcher);
+
+        }
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.addData:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.delData:
+
+                break;
+            case R.id.update:
+
+                break;
+            case R.id.setting:
+
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return true;
     }
 }
